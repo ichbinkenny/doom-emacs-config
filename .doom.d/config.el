b3ba7a80bab41083d9891be6ce499fd5359c920f
Kenneth Hunter <kennethdhunter@gmail.com>
2022-04-02 13:54:49 -0600

    Add initial emacs setup

---
+++ .doom.d/config.el
@@ -0,0 +1,93 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Kenneth Hunter"
+      user-mail-address "kennethdhunter@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq org-roam-directory "~/org_roam_files")
+(require 'company-org-roam)
+(use-package company-org-roam
+  :when (featurep! :completion company)
+  :after org-roam
+  :config
+  (set-company-backend! 'org-mode '(company-org-roam-company-yasnippet company-dabbrev)))
+
+(load-theme 'doom-material-dark t)
+
+
+;; Add additional include directories for C so headers are found
+(setq cc-search-directories '("."
+                              "/home/kenny/CLibs/STC/include"
+                              "/usr/include"
+                              "/usr/local/include/*"))
---
+++ .doom.d/custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#5a5475" "#CC6666" "#C2FFDF" "#FFEA00" "#55b3cc" "#FFB8D1" "#96CBFE" "#F8F8F0"])
+ '(custom-safe-themes
+   '("82ef0ab46e2e421c4bcbc891b9d80d98d090d9a43ae76eb6f199da6a0ce6a348" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(exwm-floating-border-color "#282633")
+ '(fci-rule-color "#B8A2CE")
+ '(highlight-tail-colors ((("#64657f") . 0) (("#605f82") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#464258" "#C5A3FF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#464258" "#C2FFDF"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#464258" "#656565"))
+ '(objed-cursor-color "#CC6666")
+ '(org-agenda-files
+   '("~/C Projects/pcap_interfacing/todo.org" "/home/kenny/OrgFiles/learning-basics.org"))
+ '(pdf-view-midnight-colors (cons "#F8F8F0" "#5a5475"))
+ '(rustic-ansi-faces
+   ["#5a5475" "#CC6666" "#C2FFDF" "#FFEA00" "#55b3cc" "#FFB8D1" "#96CBFE" "#F8F8F0"])
+ '(vc-annotate-background "#5a5475")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#C2FFDF")
+    (cons 40 "#d6f894")
+    (cons 60 "#eaf14a")
+    (cons 80 "#FFEA00")
+    (cons 100 "#f6dc00")
+    (cons 120 "#eece00")
+    (cons 140 "#E6C000")
+    (cons 160 "#eebd45")
+    (cons 180 "#f6ba8b")
+    (cons 200 "#FFB8D1")
+    (cons 220 "#ee9cad")
+    (cons 240 "#dd8189")
+    (cons 260 "#CC6666")
+    (cons 280 "#b26565")
+    (cons 300 "#986565")
+    (cons 320 "#7e6565")
+    (cons 340 "#B8A2CE")
+    (cons 360 "#B8A2CE")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
